5bf9cdba203ed71a0e807afbb6ecb71c83d52715
Aditay <innovationzone32@gmail.com>
2025-08-31 18:19:51 +0530

    added config.js separate file to generate migrations

---
+++ config/config.js
@@ -0,0 +1,41 @@
+require("dotenv").config({ quiet: true });
+
+// Configuration specifically for Sequelize CLI (migrations)
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl:
+        process.env.DB_SSL === "true"
+          ? {
+              require: true,
+              rejectUnauthorized: false,
+            }
+          : false,
+    },
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl:
+        process.env.DB_SSL === "true"
+          ? {
+              require: true,
+              rejectUnauthorized: false,
+            }
+          : false,
+    },
+  },
+};
